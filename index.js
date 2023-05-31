07075446da79c4869b2437910632516c95dee784
KhondokarAlMaruf <khondokaralmaruf01@gmail.com>
2023-06-01 05:39:28 +0600

    recipes data loaded

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 const port = process.env.PORT || 5000;
+
+const chefdata = require("./data/data.json");
+
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("server is running");
+});
+app.get("/chefdata", (req, res) => {
+  res.send(chefdata);
 });
 
 app.listen(port, () => {
